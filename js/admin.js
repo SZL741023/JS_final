6af542cdddfc6b85603c30b86db2a0538654b67e
SZL741023 <shengzhe.lin@icloud.com>
2024-12-01 15:40:31 +0800

    First commit

---
+++ js/admin.js
@@ -0,0 +1,210 @@
+const token = "SVqeYyIvAnM05Ou4JXdH0WGPO5H2";
+const headers = {
+  headers: {
+    authorization: token,
+  },
+};
+
+const baseUrl =
+  "https://livejs-api.hexschool.io/api/livejs/v1/admin/neillin/orders";
+
+// util function
+// convert date frame
+const convertDataFrame = (timestamp) =>
+  new Date(timestamp * 1000).toLocaleString("zh-TW", {
+    hour12: false,
+  });
+// convert paid: boolean to string
+const convertPiadToString = (paid) => (paid === true ? "已處理" : "未處理");
+
+let ordersData = [];
+// get orders data
+const getOrdersData = async () => {
+  try {
+    const response = await axios.get(baseUrl, headers);
+    ordersData = response.data.orders;
+    // console.log(ordersData);
+    renderOrderSection(ordersData);
+    covertOrderDataForC3(ordersData);
+    renderC3(orderDataForC3, colorDataForC3);
+  } catch (error) {
+    console.log(error.response.data.message);
+  }
+};
+// render order section
+const orderContent = document.querySelector(".orderPage-table-body");
+const renderOrderSection = (data) => {
+  let renderData = "";
+  data.forEach((order, index) => {
+    let productData = "";
+    order.products.forEach((product) => {
+      productData += `<p>${product.title} x ${product.quantity}</p>`;
+    });
+    renderData += `<tr>
+              <td>${order.id}</td>
+              <td>
+                <p>${order.user.name}</p>
+                <p>${order.user.tel}</p>
+              </td>
+              <td>${order.user.address}</td>
+              <td>${order.user.email}</td>
+              <td>
+                ${productData}
+              </td>
+              <td>${convertDataFrame(order.updatedAt)}</td>
+              <td class="orderStatus">
+                <a href="#" class="changeOrderStatus" data-idx="${index}">${convertPiadToString(order.paid)}</a>
+              </td>
+              <td>
+                <input type="button" class="delSingleOrder-Btn" data-idx="${index}" value="刪除" />
+              </td>
+            </tr>`;
+  });
+  orderContent.innerHTML = renderData;
+};
+
+// delete one order
+const deleteOneOrder = async (id) => {
+  try {
+    await axios.delete(`${baseUrl}/${id}`, headers);
+    alert(`刪除${id}訂單`);
+    getOrdersData();
+  } catch (error) {
+    console.log(error.response.data.message);
+  }
+};
+// change order status
+
+const changeOrderStatus = (id) => {
+  let orderData = {
+    data: {
+      id: id,
+      paid: true,
+    },
+  };
+  try {
+    axios.put(baseUrl, orderData, headers);
+    getOrdersData();
+    alert("狀態已更新");
+  } catch (error) {
+    console.log(error.response.data.message);
+  }
+};
+orderContent.addEventListener("click", (e) => {
+  if (e.target.getAttribute("class") === "delSingleOrder-Btn") {
+    const idx = e.target.getAttribute("data-idx");
+    const cartId = ordersData[idx].id;
+    deleteOneOrder(cartId);
+  } else if (e.target.getAttribute("class") === "changeOrderStatus") {
+    const idx = e.target.getAttribute("data-idx");
+    const cartId = ordersData[idx].id;
+    changeOrderStatus(cartId);
+  }
+});
+
+// delete all orders
+const deleteAllOrders = async () => {
+  try {
+    await axios.delete(`${baseUrl}`, headers);
+    alert("刪除全部訂單");
+    getOrdersData();
+  } catch (error) {
+    console.log(error.response.data.message);
+  }
+};
+const deleteAllBtn = document.querySelector(".discardAllBtn");
+deleteAllBtn.addEventListener("click", () => {
+  deleteAllOrders();
+});
+
+// convert data to object type with title and count
+const convertOrdersDataToObj = (ordersData) => {
+  let convertData = {};
+  ordersData.forEach((order) => {
+    order.products.forEach((product) => {
+      if (convertData[product.title] === undefined) {
+        convertData[product.title] = product.quantity;
+      } else {
+        convertData[product.title] += product.quantity;
+      }
+    });
+  });
+  return convertData;
+};
+// convert object data to c3 data type
+const convertObjDataToC3 = (objData) => {
+  const keys = Object.keys(objData);
+  let arrayDataForC3 = [];
+  keys.forEach((key) => {
+    let data = [];
+    data.push(key);
+    data.push(objData[key]);
+    arrayDataForC3.push(data);
+  });
+  arrayDataForC3.sort((a, b) => b[1] - a[1]);
+  return arrayDataForC3;
+};
+let orderDataForC3 = [];
+let colorDataForC3 = {};
+// create top3 columns type
+const createTop3ColumnsToC3 = (convertedArrayData) => {
+  const top3Data = convertedArrayData.filter((data, index) => {
+    return index < 3;
+  });
+  const othersData = convertedArrayData.filter((data, index) => {
+    return index > 2;
+  });
+  if (othersData.length !== 0) {
+    let lastData = ["其他", 0];
+    othersData.forEach((data) => {
+      lastData[1] += data[1];
+    });
+    top3Data.push(lastData);
+  }
+  console.log(top3Data);
+  return top3Data;
+};
+// create top3 color type
+const createTop3ColorToC3 = (convertedArrayData) => {
+  let colorOfTop3 = {};
+  const color = ["#DACBFF", "#9D7FEA", "#5434A7", "#301E5F"];
+  convertedArrayData.forEach((item, index) => {
+    colorOfTop3[item[0]] = color[index];
+  });
+  return colorOfTop3;
+};
+
+// convert order data for C3 render
+const covertOrderDataForC3 = (ordersData) => {
+  let productSummaryData = convertOrdersDataToObj(ordersData);
+
+  let convertedArrayData = convertObjDataToC3(productSummaryData);
+  orderDataForC3 = createTop3ColumnsToC3(convertedArrayData);
+  colorDataForC3 = createTop3ColorToC3(orderDataForC3);
+};
+
+// render c3 form
+const renderC3 = (columns, colors) => {
+  let chart = c3.generate({
+    bindto: "#chart", // HTML 元素綁定
+    data: {
+      type: "pie",
+      columns: columns,
+      colors: colors,
+    },
+  });
+};
+// initial function
+function init() {
+  getOrdersData();
+  // C3.js
+  // let chart = c3.generate({
+  //   bindto: "#chart", // HTML 元素綁定
+  //   data: {
+  //     type: "pie",
+  //     columns: orderDataForC3,
+  //     colors: colorDataForC3,
+  //   },
+  // });
+}
+init();
---
+++ js/index.js
@@ -0,0 +1,290 @@
+// api information
+const baseUrl =
+  "https://livejs-api.hexschool.io/api/livejs/v1/customer/neillin/";
+const productUrl = "products";
+const cartsUrl = "carts";
+const orderUrl = "orders";
+
+// producttion data
+let productData = [];
+// get products data
+const getProductsData = async () => {
+  try {
+    const response = await axios.get(`${baseUrl}${productUrl}`);
+    productData = response.data.products;
+    renderPorductionSection(productData);
+  } catch (error) {
+    console.log(error.response.data.message);
+  }
+};
+
+const productUlSection = document.querySelector(".productWrap");
+// render product section
+const renderPorductionSection = (data) => {
+  const renderData = data
+    .map((item, idx) => {
+      return `<li class="productCard">
+                  <h4 class="productType">新品</h4>
+                  <img
+                    src="${item.images}"
+                    alt=""
+                  />
+                  <a href="#" class="addCardBtn" data-idx="${idx}">加入購物車</a>
+                  <h3>${item.title}</h3>
+                  <del class="originPrice">NT$${item.origin_price}</del>
+                  <p class="nowPrice">NT$${item.price}</p>
+              </li>`;
+    })
+    .join("");
+  productUlSection.innerHTML = renderData;
+};
+// filte section selector
+const category = document.querySelector(".productSelect");
+// filte category of product
+const filteProductCategory = () => {
+  let newRenderData = [];
+  if (category.value !== "全部") {
+    newRenderData = productData.filter((item) => {
+      return item.category === category.value;
+    });
+  } else {
+    newRenderData = productData;
+  }
+  renderPorductionSection(newRenderData);
+};
+
+// carts data
+let cartsData = [];
+// get carts data
+const getCartsData = async () => {
+  try {
+    const response = await axios.get(`${baseUrl}${cartsUrl}`);
+    cartsData = response.data.carts;
+    renderCartsSection(cartsData);
+  } catch (error) {
+    console.log(error.response.data.message);
+  }
+};
+
+const cartsSection = document.querySelector(".shoppingCart-table");
+// render carts section
+const renderCartsSection = (data) => {
+  let contentData = "";
+  let totalPrice = 0;
+
+  data.forEach((item) => {
+    totalPrice += item.product.price * item.quantity;
+    contentData += `<tr>
+              <td>
+                <div class="cardItem-title">
+                  <img src="${item.product.images}" alt="" />
+                  <p>${item.product.title}</p>
+                </div>
+              </td>
+              <td>NT$${item.product.price}</td>
+              <td>${item.quantity}</td>
+              <td>NT$${item.product.price * item.quantity}</td>
+              <td class="discardBtn" >
+                <a href="#" class="material-icons" data-id="${item.id}"> clear </a>
+              </td>
+            </tr> `;
+  });
+  const titleData = `<tr>
+            <th width="40%">品項</th>
+            <th width="15%">單價</th>
+            <th width="15%">數量</th>
+            <th width="15%">金額</th>
+            <th width="15%"></th>
+          </tr>`;
+  const buttonData = `<tr>
+            <td>
+              <a href="#" class="discardAllBtn">刪除所有品項</a>
+            </td>
+            <td></td>
+            <td></td>
+            <td>
+              <p>總金額</p>
+            </td>
+            <td>NT$${totalPrice}</td>`;
+  const renderData = titleData + contentData + buttonData;
+  cartsSection.innerHTML = renderData;
+};
+
+// patch carts data
+
+const patchCartsData = async (data) => {
+  try {
+    const response = await axios.patch(`${baseUrl}${cartsUrl}`, data);
+    console.log(response);
+    getCartsData();
+  } catch (error) {
+    console.log(error.response.data.message);
+  }
+};
+// post carts data
+
+const postCartsData = async (data) => {
+  try {
+    await axios.post(`${baseUrl}${cartsUrl}`, data);
+    getCartsData();
+  } catch (error) {
+    console.log(error.response.data.message);
+  }
+};
+const productCard = document.querySelector(".productWrap");
+// listener for add carts button
+productCard.addEventListener("click", function (e) {
+  if (e.target.getAttribute("class") !== "addCardBtn") {
+    return;
+  }
+  const idx = e.target.getAttribute("data-idx");
+  let cartData = cartsData.filter((item) => {
+    return item.product.id === productData[idx].id;
+  });
+  if (cartData.length !== 0) {
+    let patchData = {
+      data: {
+        id: cartData[0]["id"],
+        quantity: cartData[0]["quantity"] + 1,
+      },
+    };
+    patchCartsData(patchData);
+    alert("更新購物車");
+  } else {
+    let postData = {
+      data: {
+        productId: productData[idx].id,
+        quantity: 1,
+      },
+    };
+    postCartsData(postData);
+    alert("加入購物車");
+  }
+});
+
+// delete item in carts
+const deleteCartsItem = async (id) => {
+  try {
+    await axios.delete(`${baseUrl}${cartsUrl}/${id}`);
+    getCartsData();
+  } catch (error) {
+    console.log(error.response.data.message);
+  }
+};
+
+// delete all items in carts
+
+const deleteAllCartsItem = async () => {
+  try {
+    await axios.delete(`${baseUrl}${cartsUrl}`);
+    getCartsData();
+  } catch (error) {
+    alert(error.response.data.message);
+  }
+};
+
+const cartsTable = document.querySelector(".shoppingCart-table");
+
+// listen if delete btn of item id click or not
+cartsTable.addEventListener("click", (e) => {
+  // console.log(e.target.getAttribute("class"));
+  if (e.target.getAttribute("class") === "material-icons") {
+    const id = e.target.getAttribute("data-id");
+    deleteCartsItem(id);
+  } else if (e.target.getAttribute("class") === "discardAllBtn") {
+    deleteAllCartsItem();
+    alert("清空購物車");
+  }
+});
+
+// post order to backend
+
+const orderForm = document.querySelector(".orderInfo-form");
+
+category.addEventListener("change", filteProductCategory);
+
+// initial the order information empty alert message
+const formsAlert = document.querySelectorAll(".orderInfo-message");
+const initialOrderInfoMessage = () => {
+  formsAlert.forEach((item) => {
+    item.innerHTML = "";
+  });
+};
+
+const orderInputs = document.querySelectorAll(".orderInfo-input");
+const postOrderBtn = document.querySelector(".orderInfo-btn");
+
+const orderInfoData = {
+  data: {
+    user: {
+      name: "",
+      tel: "",
+      email: "",
+      address: "",
+      payment: "",
+    },
+  },
+};
+
+// extract post data from input value
+const extractInputData = (item) => {
+  if (item.getAttribute("id") === "customerName") {
+    orderInfoData["data"]["user"]["name"] = item.value;
+  } else if (item.getAttribute("id") === "customerPhone") {
+    orderInfoData["data"]["user"]["tel"] = item.value;
+  } else if (item.getAttribute("id") === "customerEmail") {
+    orderInfoData["data"]["user"]["email"] = item.value;
+  } else if (item.getAttribute("id") === "customerAddress") {
+    orderInfoData["data"]["user"]["address"] = item.value;
+  } else if (item.getAttribute("id") === "tradeWay") {
+    orderInfoData["data"]["user"]["payment"] = item.value;
+  }
+};
+// check input is empty or not
+const checkInputDataEmpty = () => {
+  let result = true;
+  orderInputs.forEach((item) => {
+    extractInputData(item);
+    if (item.id === "tradeWay") {
+      return;
+    }
+    if (item.value === "") {
+      item.parentNode.querySelector(".orderInfo-message").innerHTML = "必填";
+      result = false;
+      return;
+    }
+    item.parentNode.querySelector(".orderInfo-message").innerHTML = "";
+  });
+  return result;
+};
+// post order to backend
+const postOrderData = async (data) => {
+  try {
+    await axios.post(`${baseUrl}${orderUrl}`, data);
+    alert("成功送出訂單!");
+  } catch (error) {
+    console.log(error.response.data.message);
+  }
+  getCartsData();
+};
+// listen if btm has been click
+orderForm.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const currentClass = e.target.getAttribute("class");
+  if (currentClass === "orderInfo-btn") {
+    const result = checkInputDataEmpty();
+    console.log(result);
+    if (result === true) {
+      postOrderData(orderInfoData);
+      document.querySelector(".orderInfo-form").reset();
+    }
+  }
+});
+
+// initial function
+function init() {
+  getProductsData();
+  getCartsData();
+  initialOrderInfoMessage();
+}
+init();
